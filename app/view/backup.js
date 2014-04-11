ec0025fa63fabbc0da486347919d139cd4810e22
Dennis Passway <dennispassway@gmail.com>
2014-04-11 12:27:48 +0200

    Troep verwijderen pt 2

--- app/view/backup.js
+++
@@ -1,9 +0,0 @@
-  // Vloer
-/*  var vloerGeometry = new THREE.PlaneGeometry(cubeGroote, cubeGroote);
-  vloerGeometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
-  var vloerBump = THREE.ImageUtils.loadTexture('img/bumpmap.jpg');
-  var vloerBumpScale = 5;
-  var vloerMaterial = new THREE.MeshPhongMaterial( {color: 0xDEDEDE, bumpMap: vloerBump, bumpScale: vloerBumpScale} );
-  vloer = new THREE.Mesh( vloerGeometry, vloerMaterial );
-  vloer.position.set(0,-50,0);
-  scene.add( vloer );*/
