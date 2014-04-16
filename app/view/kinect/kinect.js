043fa42ca12e4b7be2f16ab1600116cb49ff0a48
Dennis Passway <dennispassway@gmail.com>
2014-04-16 16:37:19 +0200

    kinect box fix

--- app/view/kinect/kinect.js
+++ app/view/kinect/kinect.js
@@ -3,7 +3,7 @@ var box;
 function initKinect() {
   var boxGeo = new THREE.CubeGeometry( 500,500,500 );
   var boxMaterial = new THREE.MeshBasicMaterial( {color: 0xff0000} );
-  var box = new THREE.Mesh(boxGeo, boxMaterial);
+  box = new THREE.Mesh(boxGeo, boxMaterial);
   scene.add(box);
   box.position.set(2000,2000,0);
 }
