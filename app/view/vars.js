19f33334c80e416423313c8078f4261a0802af40
Dennis Passway <dennispassway@gmail.com>
2014-04-18 13:00:15 +0200

    Nieuwe bounding box

--- app/view/vars.js
+++ app/view/vars.js
@@ -1,18 +1,18 @@
-var startFogAfstand = vliegWaarde;
 var landWaarde = 0.00130;
-var vliegWaarde = 0.00025;
-var densityStijging = 0.000105;
+var vliegWaarde = 0.00003;
+var densityStijging = 0.000127;
+var startFogAfstand = vliegWaarde;
 
 var fov = 40;
 var near = 1;
-var far = 20000;
+var far = 78000;
 
 var widthScreen1 = 1920;
 var heightScreen1 = 1080;
 var widthScreen2 = 1920;
 var heightScreen2 = 1080;
 
-var boundingBoxScale = 800;
+var boundingBoxScale = 5000;
 
 var floatingTweenAfstand = 400;
 
