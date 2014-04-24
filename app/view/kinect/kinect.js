c2db20398965a881ecbc4413c5a2fa9d306700a0
Dennis Passway <dennispassway@gmail.com>
2014-04-24 15:55:55 +0200

    Kinect Optimalisatie: box-clone en alleen als data is runnen

--- app/view/kinect/kinect.js
+++ app/view/kinect/kinect.js
@@ -2,11 +2,11 @@ var box;
 var box2;
 
 function initKinect() {
-  var boxGeo = new THREE.CubeGeometry( 200,200,200 );
-  var boxMaterial = new THREE.MeshBasicMaterial( {color: 0xff0000} );
-  var boxMaterial2 = new THREE.MeshBasicMaterial( {color: 0xFFCC33} );
+  boxGeo = new THREE.CubeGeometry( 200,200,200 );
+  boxMaterial = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+  boxMaterial2 = new THREE.MeshBasicMaterial( {color: 0xFFCC33} );
   box = new THREE.Mesh(boxGeo, boxMaterial);
-  box2 = new THREE.Mesh(boxGeo, boxMaterial);
+  box2 = box.clone();
   scene.add(box);
   box.add(box2);
   box.position.set(camera.position.x,camera.position.y,camera.position.z);
@@ -14,11 +14,10 @@ function initKinect() {
 }
 
 // Processingfunctie
-function processingVerwerks() {
-	box.position.set(camera.position.x,camera.position.y,camera.position.z);
-	box.rotation.set(camera.rotation.x, camera.rotation.y, camera.rotation.z);
-  	
+function processingVerwerks() { 
   if(processingData) {
+    box.position.set(camera.position.x,camera.position.y,camera.position.z);
+    box.rotation.set(camera.rotation.x, camera.rotation.y, camera.rotation.z);
     box2.position.set(closestX-500, closestY-400, -closestZ);
   }
 }
